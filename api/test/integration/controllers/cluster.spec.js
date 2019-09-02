5549cfd3efb2d04c0af4127fdeebf1c3ffdfb558
Daniel Rataj <daniel.rataj@mall.cz>
2019-09-02 13:30:17 +0200

    Add missing test for cluster

--- api/test/integration/controllers/cluster.spec.js
+++ api/test/integration/controllers/cluster.spec.js
@@ -3,23 +3,31 @@ let faker = require('faker')
 let cluster
 let createCluster = require('../../helpers/create-cluster')
 let createNodeInCluster = require('../../helpers/create-node-in-cluster')
+let clusterParams = {
+  name: faker.lorem.words(3),
+  ssh: `ssh`,
+  user: `admin`,
+  password: `admin`,
+  authKey: `authKey`,
+  authenticationDatabase: `admin`
+}
 
 describe('Cluster', () => {
   it('create cluster', (done) => {
     supertest(sails.hooks.http.app)
       .post('/v1/cluster')
-      .send({
-        name: faker.lorem.words(3),
-        ssh: `ssh`,
-        user: `admin`,
-        password: `admin`,
-        authKey: `authKey`,
-        authenticationDatabase: `admin`
-      })
+      .send(clusterParams)
       .expect((res) => {
         cluster = res.body.data
       })
       .expect(200, done)
+  })
+
+  it('create cluster that already exists', (done) => {
+    supertest(sails.hooks.http.app)
+      .post('/v1/cluster')
+      .send(clusterParams)
+      .expect(409, done)
   })
 
   it('find all clusters', (done) => {
@@ -38,15 +46,20 @@ describe('Cluster', () => {
     supertest(sails.hooks.http.app)
       .patch(`/v1/cluster`)
       .send({
-        id: cluster.id,
-        name: faker.lorem.words(4),
-        ssh: `ssh`,
-        user: `admin`,
-        password: `admin`,
-        authKey: `authKey`,
-        authenticationDatabase: `admin`
+        ...clusterParams,
+        id: cluster.id
       })
       .expect(200, done)
+  })
+
+  it(`update cluster that does not exists`, (done) => {
+    supertest(sails.hooks.http.app)
+      .patch(`/v1/cluster`)
+      .send({
+        ...clusterParams,
+        id: `some random id`
+      })
+      .expect(404, done)
   })
 
   it(`delete cluster`, (done) => {
